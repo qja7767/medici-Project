73fc961fbcf1ccbe7bbee529d99c613537745864
qja7767 <101855116+qja7767@users.noreply.github.com>
2022-07-14 23:20:32 +0900

    Delete medici directory

--- medici/src/main/java/com/arang/board/cmd/BCommand.java
+++
@@ -1,8 +0,0 @@
-package com.arang.board.cmd;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface BCommand {
-	void execute(HttpServletRequest request, HttpServletResponse response);
-}
